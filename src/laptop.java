33209dad456260271e50457b47dd609e47d72313
Gorets147 <mih.kom1998@gmail.com>
2024-01-20 19:16:09 +0300

    feat: add field for laptop

---
+++ src/laptop.java
@@ -0,0 +1,18 @@
+public class laptop {
+
+    private String name;
+
+    private String color;
+
+    private String bodyMaterial;
+
+    private Double screenDiagonal;
+
+    private int ram;
+
+    private int ssd;
+
+    private int hdd;
+
+    private String operatingSystem;
+}
